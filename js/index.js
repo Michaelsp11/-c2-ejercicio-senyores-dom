9ce7f7a77e2159b330c2c4aeb7eec7e42c7d21b8
Michaelsp11 <michaelsalgadop11@gmail.com>
2021-05-31 19:35:50 +0200

    Favicon + js ya genera el texto en la inicial y el alt en la img

--- js/index.js
+++ js/index.js
@@ -2,15 +2,19 @@ import { senyores } from "../datos/senyores.js";
 
 const principal = document.querySelector(".principal");
 
-for (const { nombre, foto } of senyores) {
-  const senyorElemento = document
-    .querySelector(".senyor-molde")
-    .cloneNode(true);
-  senyorElemento.classList.remove("senyor-molde");
-  const nombreElemento = senyorElemento.querySelector(".nombre-senyor");
-  nombreElemento.textContent = nombre;
-  const imagen = senyorElemento.querySelector(".avatar > img");
-  imagen.src = `img/${foto}`;
-
-  principal.append(senyorElemento);
+for (const { nombre, foto, profesion, estado, twitter, marcado }
+    of senyores) {
+    const senyorElemento = document
+        .querySelector(".senyor-molde")
+        .cloneNode(true);
+    senyorElemento.classList.remove("senyor-molde");
+    const nombreElemento = senyorElemento.querySelector(".nombre-senyor");
+    nombreElemento.textContent = nombre;
+    const imagen = senyorElemento.querySelector(".avatar > img");
+    imagen.src = `img/${foto}`;
+    imagen.alt = `${nombre} senyalando`;
+    const inicial = senyorElemento.querySelector(".inicial");
+    inicial.textContent =
+        nombre.toLowerCase().slice(0, 2) === "el" ? nombre.slice(3, 4) : nombre[0];
+    principal.append(senyorElemento);
 }
